33f378f694592eedaa0e19a756caa31a812508ef
Naookie Sato <necrosato@live.com>
2018-04-18 20:57:12 -0700

    added define

--- test_mpu_6050_esp32/test_mpu_6050_esp32.ino
+++ test_mpu_6050_esp32/test_mpu_6050_esp32.ino
@@ -1,6 +1,7 @@
 
 // I2Cdev and MPU6050 must be installed as libraries, or else the .cpp/.h files
 // for both classes must be in the include path of your project
+#define ESP32MPU
 #include "I2Cdev.h"
 
 #include "MPU6050_6Axis_MotionApps20.h"
