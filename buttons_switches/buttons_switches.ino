555b8aaa59f3c3903fab84971e2671a9fa8db355
Naookie Sato <necrosato@live.com>
2018-04-19 21:54:34 -0700

    modified buttons and switches for assignment with and readability in max

--- buttons_switches/buttons_switches.ino
+++ buttons_switches/buttons_switches.ino
@@ -1,6 +1,9 @@
 
+
+//int BPIN = 2;
 int BPIN = 2;
-int SPIN = 3;
+//int SPIN = 3;
+int SPIN = 4;
 
 void setup() {
     Serial.begin(9600);
@@ -11,10 +14,12 @@ void setup() {
 
 void loop() {
     if (digitalRead(BPIN) == HIGH) {
-        Serial.println("Button bushed");
+        //Serial.println("Button bushed");
+        Serial.println("B");
     }
     if (digitalRead(SPIN) == HIGH) {
-        Serial.println("Switch on");
+        //Serial.println("Switch on");
+        Serial.println("S");
     }
     delay(50);
 }
