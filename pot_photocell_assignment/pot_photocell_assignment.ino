c1cc87d326b44236159562b74a1946ae781ac8ac
necrosato <necrosato@live.com>
2018-04-26 09:08:55 -0700

    ready for assignment

--- pot_photocell_assignment/pot_photocell_assignment.ino
+++ pot_photocell_assignment/pot_photocell_assignment.ino
@@ -1,5 +1,12 @@
-#define POT_PIN 12
-#define PHOTO_PIN 14
+//#define ESP32
+
+#ifdef ESP32
+    #define POT_PIN 12
+    #define PHOTO_PIN 14
+#else
+    #define POT_PIN A0
+    #define PHOTO_PIN A1
+#endif
 
 void setup() {
     Serial.begin(115200);
@@ -11,46 +18,79 @@ int pmin = 10000;
 int phmin = 10000;
 int pmax = -1;
 int phmax = -1;
-void loop() {
-    int potval = analogRead(POT_PIN);
-    int photoval = analogRead(PHOTO_PIN);
-    /*
-    if (potval == 0) {
-        pmin = 0;
-    }
-    else {
-        pmin = min(potval,pmin);
-    }
-    if (photoval == 0) {
-        phmin = 0;
-    }
-    else {
-        phmin = min(photoval,phmin);
-    }
-    */
+int potvalmidi = 0;
+int potvalmidi_prev = 0;
+int photovalmidi = 0;
+int photovalmidi_prev = 0;
+int potval = 0;
+int potval_prev = 0;
+int photoval = 0;
+int photoval_prev = 0;
+
+void set_min_max() {
     pmin = min(potval,pmin);
     phmin = min(photoval,phmin);
     pmax = max(potval,pmax);
     phmax = max(photoval,phmax);
-    //int potvalmidi = map(potval, pmin, pmax, 0, 127);
-    //int photovalmidi = map(photoval, phmin, phmax, 0, 127); 
+}
+
+void serial_pot_log() {
     Serial.print("Potentiometer(raw, mapped): \t");
     Serial.print(potval);
     Serial.print("\t");
-    //Serial.print(potvalmidi);
+    Serial.print(potvalmidi);
     Serial.print("\tMin:\t");
     Serial.print(pmin);
     Serial.print("\tMax:\t");
     Serial.print(pmax);
     Serial.println();
+}
+void serial_photo_log() {
     Serial.print("Photocell(raw, mapped): \t");
     Serial.print(photoval);
     Serial.print("\t");
-    //Serial.print(photovalmidi);
+    Serial.print(photovalmidi);
     Serial.print("\tMin:\t");
     Serial.print(phmin);
     Serial.print("\tMax:\t");
     Serial.print(phmax);
     Serial.println();
+}
+
+void serial_pot_midi_data() {
+    Serial.print("PO ");
+    Serial.print(potvalmidi);
+    Serial.println();
+}
+void serial_photo_midi_data() {
+    Serial.print("PH ");
+    Serial.print(photovalmidi);
+    Serial.println();
+}
+
+void loop() {
+    potval = analogRead(POT_PIN);
+    photoval = analogRead(PHOTO_PIN);
+    set_min_max();
+    if (pmin < pmax) {
+        potvalmidi  = map(potval, pmin, pmax, 0, 127);
+    }
+    if (phmin < phmax) {
+        photovalmidi = map(photoval, phmin, phmax, 0, 127); 
+    }
+    //if (potval != potval_prev) {
+    if (potvalmidi != potvalmidi_prev) {
+        //serial_pot_log();
+        serial_pot_midi_data();
+    }
+    //if (photoval != photoval_prev) {
+    if (photovalmidi != photovalmidi_prev) {
+        //serial_photo_log();
+        serial_photo_midi_data();
+    }
+    potval_prev = potval;
+    potvalmidi_prev = potvalmidi;
+    photoval_prev = photoval;
+    photovalmidi_prev = photovalmidi;
     delay(100);
 }
