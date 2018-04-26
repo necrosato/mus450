6c0fd6392dc8fed33fee1b03a294fbd3f023e988
necrosato <necrosato@live.com>
2018-04-25 17:47:36 -0700

    updated ino

--- pot_photocell_assignment/pot_photocell_assignment.ino
+++ pot_photocell_assignment/pot_photocell_assignment.ino
@@ -1,6 +1,9 @@
 #define POT_PIN 12
 #define PHOTO_PIN 14
 
+#define min(a,b) ((a)<(b)?(a):(b));
+#define max(a,b) ((a)>(b)?(a):(b));
+
 void setup() {
     Serial.begin(115200);
     delay(1000);
@@ -14,13 +17,13 @@ int phmax = -1;
 void loop() {
     int potval = analogRead(POT_PIN);
     int photoval = analogRead(PHOTO_PIN);
-    pmin = min(pmin, potval);
-    phmin = min(phmin, photoval);
-    pmax = max(pmax, potval);
-    phmax = max(phmax, photoval);
+    pmin = min(potval,pmin);
+    phmin = min(photoval,phmin);
+    pmax = max(potval,pmax);
+    phmax = max(photoval,phmax);
     int potvalmidi = map(potval, pmin, pmax, 0, 127);
     int photovalmidi = map(photoval, phmin, phmax, 0, 127); 
-    Serial.print("Poteniometer(raw, mapped): \t");
+    Serial.print("Potentiometer(raw, mapped): \t");
     Serial.print(potval);
     Serial.print("\t");
     Serial.print(potvalmidi);
