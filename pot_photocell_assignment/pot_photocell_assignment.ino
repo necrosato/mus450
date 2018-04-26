4c647a12aa1abe70e3a58fe3c9dceec654f83d1e
necrosato <necrosato@live.com>
2018-04-25 18:57:50 -0700

    asdf

--- pot_photocell_assignment/pot_photocell_assignment.ino
+++ pot_photocell_assignment/pot_photocell_assignment.ino
@@ -1,9 +1,6 @@
 #define POT_PIN 12
 #define PHOTO_PIN 14
 
-#define min(a,b) ((a)<(b)?(a):(b));
-#define max(a,b) ((a)>(b)?(a):(b));
-
 void setup() {
     Serial.begin(115200);
     delay(1000);
@@ -17,16 +14,30 @@ int phmax = -1;
 void loop() {
     int potval = analogRead(POT_PIN);
     int photoval = analogRead(PHOTO_PIN);
+    /*
+    if (potval == 0) {
+        pmin = 0;
+    }
+    else {
+        pmin = min(potval,pmin);
+    }
+    if (photoval == 0) {
+        phmin = 0;
+    }
+    else {
+        phmin = min(photoval,phmin);
+    }
+    */
     pmin = min(potval,pmin);
     phmin = min(photoval,phmin);
     pmax = max(potval,pmax);
     phmax = max(photoval,phmax);
-    int potvalmidi = map(potval, pmin, pmax, 0, 127);
-    int photovalmidi = map(photoval, phmin, phmax, 0, 127); 
+    //int potvalmidi = map(potval, pmin, pmax, 0, 127);
+    //int photovalmidi = map(photoval, phmin, phmax, 0, 127); 
     Serial.print("Potentiometer(raw, mapped): \t");
     Serial.print(potval);
     Serial.print("\t");
-    Serial.print(potvalmidi);
+    //Serial.print(potvalmidi);
     Serial.print("\tMin:\t");
     Serial.print(pmin);
     Serial.print("\tMax:\t");
@@ -35,7 +46,7 @@ void loop() {
     Serial.print("Photocell(raw, mapped): \t");
     Serial.print(photoval);
     Serial.print("\t");
-    Serial.print(photovalmidi);
+    //Serial.print(photovalmidi);
     Serial.print("\tMin:\t");
     Serial.print(phmin);
     Serial.print("\tMax:\t");
