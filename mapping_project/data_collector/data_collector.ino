71e3ebf10ba39f2079d04e273c3dce584dac14f1
Naookie Sato <necrosato@live.com>
2018-04-29 14:01:13 -0700

    serial perser one hunnid

--- mapping_project/data_collector/data_collector.ino
+++ mapping_project/data_collector/data_collector.ino
@@ -3,12 +3,13 @@
 void serial_test_0() {
     delay(1000);
     char channel[2] = {0};
-    char data[3] = {0};
+    char data[4] = {0};
     for (int i = 0; i < 40; i++) {
         channel[0] = (char) (i / 10);
         channel[1] = (char) (i % 10);
         Serial.print(channel[0]);
         Serial.print(channel[1]);
+        Serial.print((char)0);
         Serial.print((char)0);
         Serial.print((char)0);
         Serial.print((char)0);
@@ -19,18 +20,20 @@ void serial_test_0() {
         channel[0] = (char) (i / 10);
         channel[1] = (char) (i % 10);
         for (int j = 0; j < 127; j++) {
-            data[0] = j / 100;
-            data[1] = j / 10;
-            data[2] = j % 10;
+            data[1] = j / 100;
+            data[2] = (j % 100) / 10;
+            data[3] = j % 10;
             Serial.print(channel[0]);
             Serial.print(channel[1]);
             Serial.print(data[0]);
             Serial.print(data[1]);
             Serial.print(data[2]);
+            Serial.print(data[3]);
             Serial.println();
         }
         Serial.print(channel[0]);
         Serial.print(channel[1]);
+        Serial.print((char)0);
         Serial.print((char)0);
         Serial.print((char)0);
         Serial.print((char)0);
@@ -42,6 +45,7 @@ void serial_test_0() {
         channel[1] = (char) (i % 10);
         Serial.print(channel[0]);
         Serial.print(channel[1]);
+        Serial.print((char)0);
         Serial.print((char)0);
         Serial.print((char)0);
         Serial.print((char)0);
@@ -52,7 +56,7 @@ void serial_test_0() {
 void serial_test_1() {
     delay(1000);
     char channel[2] = {0};
-    char data[3] = {0};
+    char data[4] = {0};
     for (int i = 0; i < 40; i++) {
         channel[0] = (char) (i / 10);
         channel[1] = (char) (i % 10);
@@ -61,13 +65,14 @@ void serial_test_1() {
         Serial.print((char)0);
         Serial.print((char)0);
         Serial.print((char)0);
+        Serial.print((char)0);
         Serial.println();
     }
     delay(1000);
     for (int j = 0; j < 127; j++) {
-        data[0] = j / 100;
-        data[1] = j / 10;
-        data[2] = j % 10;
+        data[1] = j / 100;
+        data[2] = (j % 100) / 10;
+        data[3] = j % 10;
         for (int i = 0; i < 40; i++) {
             channel[0] = (char) (i / 10);
             channel[1] = (char) (i % 10);
@@ -76,6 +81,7 @@ void serial_test_1() {
             Serial.print(data[0]);
             Serial.print(data[1]);
             Serial.print(data[2]);
+            Serial.print(data[3]);
             Serial.println();
         }
     }
@@ -85,6 +91,7 @@ void serial_test_1() {
         channel[1] = (char) (i % 10);
         Serial.print(channel[0]);
         Serial.print(channel[1]);
+        Serial.print((char)0);
         Serial.print((char)0);
         Serial.print((char)0);
         Serial.print((char)0);
