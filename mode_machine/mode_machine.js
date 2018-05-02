1f8f3475c1acbdc9a33cf1ad5595a0496e4c85e5
necrosato <necrosato@live.com>
2018-05-02 09:35:28 -0700

    mode machine works with diminished, augmented, and arbitrary scales

--- mode_machine/mode_machine.js
+++ mode_machine/mode_machine.js
@@ -1,15 +1,26 @@
 var scales = {'major': 			[1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1],
 			  'harmonic_minor': [1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 0, 1],
-			  'melodic_minor': 	[1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1]};
+			  'melodic_minor': 	[1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+              'diminished':     [1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0],
+              'augmented':      [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0] 
+             };
+
+function get_mode_count(scale) {
+    var mode_count = 0;
+    for (var i = 0; i < scale.length; i++) {
+        if (scales[scale][i] == 1) {
+            mode_count++;
+        }
+    }
+    return mode_count;
+}
 
 function get_mode_offset(n, scale) {
 	//function returns the offset into scale (0 indexed) for a given mode n of a scale
 	var ncount = 0;
     var target = ((n-1) % 7)+1;
-    var mult = Math.floor((n-1)/7);
-    if (scale == 'diminished') { 
-        mult = Math.floor((n-1)/4);
-    }
+    var mode_count = get_mode_count(scale);
+    var mult = Math.floor((n-1)/mode_count);
 	for (var i = 0; i < 12; i++) {
 		if (scales[scale][i] == 1) {
 			ncount++;
@@ -48,15 +59,27 @@ function get_chord(n, depth, mode, root, scale) {
     // scale: key in scales
 	var root_mode = get_mode(mode, root, scale);
     var mcount = root_mode.length;
-	var mode_offset = root_mode[(n-1)%7] - root_mode[0];
-	var chord_scale = get_mode(((mode+n-2)%7)+1, root+mode_offset, scale);
-	var chord_mult = Math.floor((n - 1) / 7);
+	var mode_offset = root_mode[(n-1)%mcount] - root_mode[0];
+	var chord_scale = get_mode(((mode+n-2)%mcount)+1, root+mode_offset, scale);
+	var chord_mult = Math.floor((n - 1) / mcount);
     var chord = [];
 	for (var i = 0; i < depth; i++) {
-		var index = (i * 2) % 7;
-        var note_mult = Math.floor((i * 2) / 7);
+		var index = (i * 2) % mcount;
+        var note_mult = Math.floor((i * 2) / mcount);
+        if ((i * 2 >= mcount) && (mcount % 2 == 0) && (note_mult % 2 == 1)) {
+            // this should only apply to diminished and augmented scales
+            index++;
+        }
         chord.push(chord_scale[index]+(note_mult*12)+(chord_mult*12)); 
 	}
 	outlet(0, chord);
     return chord;
 }
+
+//console.log(get_mode(1, 60, 'diminished'));
+//console.log(get_chord(1, 4, 1, 60, 'diminished'));
+//console.log(get_chord(1, 8, 1, 60, 'diminished'));
+//console.log(get_chord(1, 7, 1, 60, 'major'));
+//console.log(get_chord(1, 14, 1, 60, 'major'));
+//console.log(get_chord(1, 6, 1, 60, 'augmented'));
+//console.log(get_chord(1, 12, 1, 60, 'augmented'));
