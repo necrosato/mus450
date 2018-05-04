ce97421a4a5f45f42c8401029156004d089a7db9
necrosato <necrosato@live.com>
2018-05-04 11:00:07 -0700

    more work

--- mapping_project/data_collector/data_collector.ino
+++ mapping_project/data_collector/data_collector.ino
@@ -22,12 +22,12 @@ MPU6050 accelgyro;
 #define AG_CONST
 #define AG_MIN -32768
 #define AG_MAX 32767
-#define AX_CHANNEL 2
-#define AY_CHANNEL 3
-#define AZ_CHANNEL 4
-#define GX_CHANNEL 5
-#define GY_CHANNEL 6
-#define GZ_CHANNEL 7
+#define AX_CHANNEL 6
+#define AY_CHANNEL 7
+#define AZ_CHANNEL 8
+#define GX_CHANNEL 9
+#define GY_CHANNEL 10
+#define GZ_CHANNEL 11
 int16_t ax = 0;
 int16_t ay = 0;
 int16_t az = 0;
@@ -35,82 +35,137 @@ int16_t gx = 0;
 int16_t gy = 0;
 int16_t gz = 0;
 #ifdef AG_CONST
-    int16_t axmin = AG_MIN;
-    int16_t axmax = AG_MAX;
-    int16_t aymin = AG_MIN;
-    int16_t aymax = AG_MAX;
-    int16_t azmin = AG_MIN;
-    int16_t azmax = AG_MAX;
-    int16_t gxmin = AG_MIN;
-    int16_t gxmax = AG_MAX;
-    int16_t gymin = AG_MIN;
-    int16_t gymax = AG_MAX;
-    int16_t gzmin = AG_MIN;
-    int16_t gzmax = AG_MAX;
+    int16_t ax_min = AG_MIN;
+    int16_t ax_max = AG_MAX;
+    int16_t ay_min = AG_MIN;
+    int16_t ay_max = AG_MAX;
+    int16_t az_min = AG_MIN;
+    int16_t az_max = AG_MAX;
+    int16_t gx_min = AG_MIN;
+    int16_t gx_max = AG_MAX;
+    int16_t gy_min = AG_MIN;
+    int16_t gy_max = AG_MAX;
+    int16_t gz_min = AG_MIN;
+    int16_t gz_max = AG_MAX;
 #else
-    int16_t axmin = 0;
-    int16_t axmax = 0;
-    int16_t aymin = 0;
-    int16_t aymax = 0;
-    int16_t azmin = 0;
-    int16_t azmax = 0;
-    int16_t gxmin = 0;
-    int16_t gxmax = 0;
-    int16_t gymin = 0;
-    int16_t gymax = 0;
-    int16_t gzmin = 0;
-    int16_t gzmax = 0;
+    int16_t ax_min = 0;
+    int16_t ax_max = 0;
+    int16_t ay_min = 0;
+    int16_t ay_max = 0;
+    int16_t az_min = 0;
+    int16_t az_max = 0;
+    int16_t gx_min = 0;
+    int16_t gx_max = 0;
+    int16_t gy_min = 0;
+    int16_t gy_max = 0;
+    int16_t gz_min = 0;
+    int16_t gz_max = 0;
 #endif
-int16_t axmapped = 0;
-int16_t aymapped = 0;
-int16_t azmapped = 0;
-int16_t gxmapped = 0;
-int16_t gymapped = 0;
-int16_t gzmapped = 0;
+int16_t ax_mapped = 0;
+int16_t ay_mapped = 0;
+int16_t az_mapped = 0;
+int16_t gx_mapped = 0;
+int16_t gy_mapped = 0;
+int16_t gz_mapped = 0;
+
+#define ANALOG_MIN 0
+#ifdef ESP32
+    #define ANALOG_MAX 4095
+#else
+    #define ANALOG_MAX 1023
+#endif
+#define FSR0_CHANNEL 15     // nut fsr
+#define FSR1_CHANNEL 2      // pick fsr
+#define FSR2_CHANNEL 4      // neck fsr
+#define HOTPOT_CHANNEL 5    // HotPot
+#define SOFTPOT0_CHANNEL 18 // SoftPot leads to head
+#define SOFTPOT1_CHANNEL 18 // SoftPot leads to body
+int16_t fsr0 = 0;
+int16_t fsr1 = 0;
+int16_t fsr2 = 0;
+int16_t hotpot = 0;
+int16_t softpot0 = 0;
+int16_t softpot1 = 0;
+int16_t fsr0_min = ANALOG_MIN;
+int16_t fsr1_min = ANALOG_MIN;
+int16_t fsr2_min = ANALOG_MIN;
+int16_t hotpot_min = ANALOG_MIN;
+int16_t softpot0_min = ANALOG_MIN;
+int16_t softpot1_min = ANALOG_MIN;
+int16_t fsr0_max = ANALOG_MAX;
+int16_t fsr1_max = ANALOG_MAX;
+int16_t fsr2_max = ANALOG_MAX;
+int16_t hotpot_max = ANALOG_MAX;
+int16_t softpot0_max = ANALOG_MAX;
+int16_t softpot1_max = ANALOG_MAX;
+int16_t fsr0_mapped = 0;
+int16_t fsr1_mapped = 0;
+int16_t fsr2_mapped = 0;
+int16_t hotpot_mapped = 0;
+int16_t softpot0_mapped = 0;
+int16_t softpot1_mapped = 0;
 
 char channel[2] = {0};
 char data[4] = {0};
 
 void get_min_max_accel() {
-    axmin = min(ax, axmin);
-    aymin = min(ay, aymin);
-    azmin = min(az, azmin);
-    axmax = max(ax, axmax);
-    aymax = max(ay, aymax);
-    azmax = max(az, azmax);
+    ax_min = min(ax, ax_min);
+    ay_min = min(ay, ay_min);
+    az_min = min(az, az_min);
+    ax_max = max(ax, ax_max);
+    ay_max = max(ay, ay_max);
+    az_max = max(az, az_max);
 }
 
 void get_min_max_gyro() {
-    gxmin = min(gx, gxmin);
-    gymin = min(gy, gymin);
-    gzmin = min(gz, gzmin);
-    gxmax = max(gx, gxmax);
-    gymax = max(gy, gymax);
-    gzmax = max(gz, gzmax);
+    gx_min = min(gx, gx_min);
+    gy_min = min(gy, gy_min);
+    gz_min = min(gz, gz_min);
+    gx_max = max(gx, gx_max);
+    gy_max = max(gy, gy_max);
+    gz_max = max(gz, gz_max);
+}
+
+void get_min_max_analogs() {
+    fsr0_min = min(fsr0, fsr0_min);
+    fsr1_min = min(fsr1, fsr1_min);
+    fsr2_min = min(fsr2, fsr2_min);
+    hotpot_min = min(hotpot, hotpot_min);
+    softpot0_min = min(softpot0, softpot0_min);
+    softpot1_min = min(softpot1, softpot1_min);
+    fsr0_max = max(fsr0, fsr0_max);
+    fsr1_max = max(fsr1, fsr1_max);
+    fsr2_max = max(fsr2, fsr2_max);
+    hotpot_max = max(hotpot, hotpot_max);
+    softpot0_max = max(softpot0, softpot0_max);
+    softpot1_max = max(softpot1, softpot1_max);
 }
 
 void map_accel() {
-    if (axmin < axmax) {
-        axmapped = map(ax, axmin, axmax, MAP_MIN, MAP_MAX);
+    if (ax_min < ax_max) {
+        ax_mapped = map(ax, ax_min, ax_max, MAP_MIN, MAP_MAX);
     }
-    if (aymin < aymax) {
-        aymapped = map(ay, aymin, aymax, MAP_MIN, MAP_MAX);
+    if (ay_min < ay_max) {
+        ay_mapped = map(ay, ay_min, ay_max, MAP_MIN, MAP_MAX);
     }
-    if (gzmin < azmax) {
-        azmapped = map(az, azmin, azmax, MAP_MIN, MAP_MAX);
+    if (gz_min < az_max) {
+        az_mapped = map(az, az_min, az_max, MAP_MIN, MAP_MAX);
     }
 }
 
 void map_gyro() {
-    if (gxmin < gxmax) {
-        gxmapped = map(gx, gxmin, gxmax, MAP_MIN, MAP_MAX);
+    if (gx_min < gx_max) {
+        gx_mapped = map(gx, gx_min, gx_max, MAP_MIN, MAP_MAX);
     }
-    if (gymin < gymax) {
-        gymapped = map(gy, gymin, gymax, MAP_MIN, MAP_MAX);
+    if (gy_min < gy_max) {
+        gy_mapped = map(gy, gy_min, gy_max, MAP_MIN, MAP_MAX);
     }
-    if (gzmin < gzmax) {
-        gzmapped = map(gz, gzmin, gzmax, MAP_MIN, MAP_MAX);
+    if (gz_min < gz_max) {
+        gz_mapped = map(gz, gz_min, gz_max, MAP_MIN, MAP_MAX);
     }
+}
+
+void map_analogs() {
 }
 
 void print_ag_raw_all() {
@@ -123,45 +178,45 @@ void print_ag_raw_all() {
     Serial.print(gy); Serial.print("\t");
     Serial.print(gz); Serial.print("\t");
     Serial.print("mins:\t");
-    Serial.print(axmin); Serial.print("\t");
-    Serial.print(aymin); Serial.print("\t");
-    Serial.print(azmin); Serial.print("\t");
-    Serial.print(gxmin); Serial.print("\t");
-    Serial.print(gymin); Serial.print("\t");
-    Serial.print(gzmin); Serial.print("\t");
+    Serial.print(ax_min); Serial.print("\t");
+    Serial.print(ay_min); Serial.print("\t");
+    Serial.print(az_min); Serial.print("\t");
+    Serial.print(gx_min); Serial.print("\t");
+    Serial.print(gy_min); Serial.print("\t");
+    Serial.print(gz_min); Serial.print("\t");
     Serial.print("maxs:\t");
-    Serial.print(axmax); Serial.print("\t");
-    Serial.print(aymax); Serial.print("\t");
-    Serial.print(azmax); Serial.print("\t");
-    Serial.print(gxmax); Serial.print("\t");
-    Serial.print(gymax); Serial.print("\t");
-    Serial.print(gzmax); Serial.print("\t");
+    Serial.print(ax_max); Serial.print("\t");
+    Serial.print(ay_max); Serial.print("\t");
+    Serial.print(az_max); Serial.print("\t");
+    Serial.print(gx_max); Serial.print("\t");
+    Serial.print(gy_max); Serial.print("\t");
+    Serial.print(gz_max); Serial.print("\t");
     Serial.println();
 }
 
 void print_ag_mapped_all() {
     // display tab-separated accel/gyro x/y/z values
     Serial.print("a/g:\t");
-    Serial.print(axmapped); Serial.print("\t");
-    Serial.print(aymapped); Serial.print("\t");
-    Serial.print(azmapped); Serial.print("\t");
-    Serial.print(gxmapped); Serial.print("\t");
-    Serial.print(gymapped); Serial.print("\t");
-    Serial.print(gzmapped); Serial.print("\t");
+    Serial.print(ax_mapped); Serial.print("\t");
+    Serial.print(ay_mapped); Serial.print("\t");
+    Serial.print(az_mapped); Serial.print("\t");
+    Serial.print(gx_mapped); Serial.print("\t");
+    Serial.print(gy_mapped); Serial.print("\t");
+    Serial.print(gz_mapped); Serial.print("\t");
     Serial.print("mins:\t");
-    Serial.print(axmin); Serial.print("\t");
-    Serial.print(aymin); Serial.print("\t");
-    Serial.print(azmin); Serial.print("\t");
-    Serial.print(gxmin); Serial.print("\t");
-    Serial.print(gymin); Serial.print("\t");
-    Serial.print(gzmin); Serial.print("\t");
+    Serial.print(ax_min); Serial.print("\t");
+    Serial.print(ay_min); Serial.print("\t");
+    Serial.print(az_min); Serial.print("\t");
+    Serial.print(gx_min); Serial.print("\t");
+    Serial.print(gy_min); Serial.print("\t");
+    Serial.print(gz_min); Serial.print("\t");
     Serial.print("maxs:\t");
-    Serial.print(axmax); Serial.print("\t");
-    Serial.print(aymax); Serial.print("\t");
-    Serial.print(azmax); Serial.print("\t");
-    Serial.print(gxmax); Serial.print("\t");
-    Serial.print(gymax); Serial.print("\t");
-    Serial.print(gzmax); Serial.print("\t");
+    Serial.print(ax_max); Serial.print("\t");
+    Serial.print(ay_max); Serial.print("\t");
+    Serial.print(az_max); Serial.print("\t");
+    Serial.print(gx_max); Serial.print("\t");
+    Serial.print(gy_max); Serial.print("\t");
+    Serial.print(gz_max); Serial.print("\t");
     Serial.println();
 }
 void send_channel_data() {
@@ -195,22 +250,22 @@ void set_data(int val) {
 
 void send_ag_mapped_all() {
     set_channel(AX_CHANNEL);
-    set_data(axmapped);
+    set_data(ax_mapped);
     send_channel_data();
     set_channel(AY_CHANNEL);
-    set_data(aymapped);
+    set_data(ay_mapped);
     send_channel_data();   
     set_channel(AZ_CHANNEL);
-    set_data(azmapped);
+    set_data(az_mapped);
     send_channel_data();
     set_channel(GX_CHANNEL);
-    set_data(gxmapped);
+    set_data(gx_mapped);
     send_channel_data();
     set_channel(GY_CHANNEL);
-    set_data(gymapped);
+    set_data(gy_mapped);
     send_channel_data();
     set_channel(GZ_CHANNEL);
-    set_data(gzmapped);
+    set_data(gz_mapped);
     send_channel_data();
 }
 
@@ -290,8 +345,6 @@ void loop() {
     get_min_max_gyro();
     map_accel();
     map_gyro();
-    //accelgyro.getAcceleration(&ax, &ay, &az);
-    //accelgyro.getRotation(&gx, &gy, &gz);
     //print_ag_raw_all();
     print_ag_mapped_all();
     //send_ag_mapped_all();
